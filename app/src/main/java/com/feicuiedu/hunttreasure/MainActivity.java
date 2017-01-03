9ca33c0cf2339c81d7d58d6fbec65b63ecacf81d
Loserlxx <983440092@qq.com>
2017-01-03 14:33:54 +0800

    注册页面实现

--- app/src/main/java/com/feicuiedu/hunttreasure/MainActivity.java
+++ app/src/main/java/com/feicuiedu/hunttreasure/MainActivity.java
@@ -1,16 +1,65 @@
 package com.feicuiedu.hunttreasure;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.feicuiedu.hunttreasure.commons.ActivityUtils;
+import com.feicuiedu.hunttreasure.user.LoginActivity;
+import com.feicuiedu.hunttreasure.user.RegisterActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String MATION_INSTANCE = "OFF_MAIN_ACTIVITY";
+    private ActivityUtils activityUtils;
+    private Unbinder bind;
+    private BroadcastReceiver mReceiver=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            finish();
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        activityUtils=new ActivityUtils(this);
+        bind = ButterKnife.bind(this);
+        IntentFilter intentFilter=new IntentFilter(MATION_INSTANCE);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver,intentFilter);
+    }
 
-        Toast.makeText(this, "修改", Toast.LENGTH_SHORT).show();
+    /**
+     * 点击事件（登录）（注册）
+     */
+    @OnClick({R.id.btn_Register, R.id.btn_Login})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_Register:
+                activityUtils.startActivity(RegisterActivity.class);
+                break;
+            case R.id.btn_Login:
+                activityUtils.startActivity(LoginActivity.class);
+                break;
+        }
+    }
+
+    /**
+     * 摧毁文件
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //关闭黄油刀
+        bind.unbind();
     }
 }
